5cfdea1e9fb98c4411fec471d560d73fd2741c01
adityap2007 <adityapetkar07@gmail.com>
2025-04-01 08:57:27 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 export default function Page() {
   return (
     <section>
+      <img src="ChatGPT Image Apr 1, 2025, 08" alt="Description" width="300" />
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Strata Mate
       </h1>
