5f4ff060015e1124955011db335ab5924a6a9ffe
adityap2007 <adityapetkar07@gmail.com>
2025-03-31 11:46:01 +1100

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,13 +2,13 @@ import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
   title: 'Commitee Members',
-  description: 'Read my blog.',
+  description: 'Committee members of the Strata Management',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Committee Members</h1>
       <BlogPosts />
     </section>
   )
