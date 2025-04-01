f435653da4bdf9ca48f77f3718a95799a99e7317
Aditya Petkar <alokpetkar@192-168-1-163.tpgi.com.au>
2025-04-01 12:18:07 +1100

    Fix: Update to pnpm and add pnpm configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    mdxRs: true,
+  },
 }
 
 module.exports = nextConfig 
